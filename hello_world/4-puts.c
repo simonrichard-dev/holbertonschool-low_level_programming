c034b686f65470681815ecc76df6cffb7d374f07
simonrichard-dev <simon.insa@gmail.com>
2022-09-30 09:35:35 -0700

    fixed 4-puts script V0

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,12 @@
-puts ("Programming is like building a multilingual puzzle);
-return 0;
+#include <stdio.h>
+
+/**
+ *main - print a sentence
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
