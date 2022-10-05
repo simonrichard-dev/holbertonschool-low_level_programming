8dd27a15f6d2e7aadba4b50b8459ac79b65e5822
simonrichard-dev <simon.insa@gmail.com>
2022-10-05 22:50:18 +0200

    fixed 5-sign.c script V0

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -11,17 +11,16 @@ int print_sign(int c)
 {
 	if (c > '0')
 	{
-		return (1);
 		_putchar('+');
+		return (1);
 	}
-	if (c < '0')
+	else if (c == '0')
 	{
-		return (-1);
-		_putchar('-');
+		return (0);
 	}
 	else
 	{
-		return (0);
-		_putchar(0);
+		_putchar('-');
+		return (-1);
 	}
 }
