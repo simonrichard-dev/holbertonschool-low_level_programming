07a9b3201dc6e14f93dfbddf99f17ad8cf49fc6f
simonrichard-dev <simon.insa@gmail.com>
2022-10-05 23:38:58 +0200

    add 6-abs.c script

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *@c : damnitsmine
+ *
+ * Return: 0 otherwise
+ *
+ */
+int _abs(int c)
+{
+	if (c >= 0)
+		return (c);
+	if (c < 0)
+		return (-c);
+	else
+		return (0);
+}
