1954c1d912036978b0c9d9a1de3d2e730a396ad4
simonrichard-dev <simon.insa@gmail.com>
2022-10-05 18:58:08 +0200

    add 2-print_alphabet_x10.c script V0

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0 (Success).
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
--- functions_nested_loops/README.md
+++ functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 0-putchar.c : program that prints _putchar, followed by a new line.
 1-alphabet.c : a function that prints the alphabet, in lowercase, followed by a new line.
+2-print_alphabet_x10.c : function that prints 10 times the alphabet, in lowercase, followed by a new line.
