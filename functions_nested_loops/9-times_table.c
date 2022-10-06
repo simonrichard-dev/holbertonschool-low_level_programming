e32373575b33e4dd760b5cee7d1737a3c92f10ac
simonrichard-dev <simon.insa@gmail.com>
2022-10-06 02:57:37 +0200

    add 9-times_table.c script V0

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * times_table - function that prints the 9 times table, starting with 0.
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int x, y, r;
+
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar('0');
+		for (y = 1; y <= 9; y++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			r = x * y;
+			if (r <= 9)
+				_putchar(' ');
+			else
+				_putchar((r / 10) + '0');
+			_putchar((r % 10) + 0);
+		}
+		_putchar('\n');
+	}
+}
--- functions_nested_loops/README.md
+++ functions_nested_loops/README.md
@@ -6,3 +6,5 @@
 5-sign.c : function that prints the sign of a number.
 6-abs.c : function that computes the absolute value of an integer.
 7-print_the_last_digit.c : function that prints the last digit of a number.
+8-24_hours.c : function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+9-times_table.c : function that prints the 9 times table, starting with 0.
