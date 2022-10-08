2ab52434ac39d80a7f3eb999853557b0ee75e48f
simonrichard-dev <simon.insa@gmail.com>
2022-10-08 22:04:30 +0200

    add 10-print_triangle.c script V0

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_triangle - function that draws a triangle.
+ *@size : size of the triangle
+ *
+ * Return: blank and #
+ */
+
+void print_triangle(int size)
+{
+	int l;
+	int c;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+
+		for (l = 0; l <= size; l++)
+		{
+			for (c = l; c < size; c++)
+			{
+				_putchar(' ');
+			}
+
+			for (c = 0; c <= l; c++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
+	}
+}
