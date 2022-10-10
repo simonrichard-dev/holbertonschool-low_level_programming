700671f3ecec04cb67815120c1ccf23d5c2c6c77
simonrichard-dev <simon.insa@gmail.com>
2022-10-10 13:41:42 +0200

    add 0-reset_to_98.c script

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int and updates value.
+ * @n : the variable
+ *
+ * Return: Always 0 (Success).
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
