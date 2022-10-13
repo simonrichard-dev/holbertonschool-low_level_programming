b8a2f87fa2e3b39a6888ccf1c6b6b48e481dc49c
simonrichard-dev <simon.insa@gmail.com>
2022-10-13 21:04:53 +0200

    add 3-strcmp.c script V0

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1 : string 1
+ * @s2 : string 2
+ *
+ * Return : s1 and s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
