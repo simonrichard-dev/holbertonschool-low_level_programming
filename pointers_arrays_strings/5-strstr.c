87cbbe07fa4a05815be701b7cb6a3ce0397c582f
simonrichard-dev <simon.insa@gmail.com>
2022-10-14 17:10:25 +0200

    add 5-strstr.c script V0

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: first string
+ * @needle: second string
+ *
+ * Return: Pointer to memory area
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		i = 0;
+
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+
+				i++;
+
+			} while (haystack[i] == needle[i]);
+		}
+
+		haystack++;
+	}
+
+	return ('\0');
+}
