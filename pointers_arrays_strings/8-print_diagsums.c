36aba5d14b4342dd666b60ef34138ef3d8899821
simonrichard-dev <simon.insa@gmail.com>
2022-10-14 18:13:16 +0200

    add 8-print_diagsums.c script V0

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_diagsums - print the sum of two diagonals of a square matrix
+ * @a: int ptr
+ * @size: int
+ */
+void	print_diagsums(int *a, int size)
+{
+	int	i;
+	int	rx;
+	int	ry;
+
+	rx = 0;
+	ry = 0;
+	for (i = 0; i < size; i++)
+	{
+		rx += a[(size + 1) * i];
+		ry += a[(size - 1) * (i + 1)];
+	}
+	printf("%d, %d\n", rx, ry);
+}
