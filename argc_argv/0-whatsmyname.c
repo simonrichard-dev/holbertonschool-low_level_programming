b892c966bdd6a7c63798f97bde631e920907c474
simonrichard-dev <simon.insa@gmail.com>
2022-10-20 14:34:36 +0200

    add 0-whatsmyname.c V0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - programm tha prints its name
+ * @argc: int variable
+ * @argv: char variable
+ *
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
