0fef82c2ca6b2ed4c210a16a5d3c0f8ed180b379
simonrichard-dev <simon.insa@gmail.com>
2022-10-27 11:31:10 +0200

    add 2-calloc.c script V0

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: number of elements
+ * @size: bytes
+ *
+ * Return: a pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i = 0;
+	char *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(size * nmemb);
+	if (p == NULL)
+		return (NULL);
+
+	while (i < nmemb)
+	{
+		p[i] = 0;
+		i++;
+	}
+	return (p);
+}
