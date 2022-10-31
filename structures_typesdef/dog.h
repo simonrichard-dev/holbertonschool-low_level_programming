23920bd96cedb8e36c220fc01d11ecccaa290e57
simonrichard-dev <simon.insa@gmail.com>
2022-10-31 11:18:11 +0100

    add files for task 0 - V0

---
+++ structures_typesdef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - definition of a new type
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of the owner
+ *
+ * Description: The Poppy structure
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
