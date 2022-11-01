46dcd9eea96567421db6da054bbd060060745fba
simonrichard-dev <simon.insa@gmail.com>
2022-11-01 16:58:38 +0100

    fixed 4-new_dog.c script V6

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,39 +2,6 @@
 #include <stdlib.h>
 #include "dog.h"
 
-/**
- * _strdup - duplicates a string
- * @str: entry point
- *
- * Return: a pointer
- */
-
-/*
-char *_strdup(char *str)
-{
-	char *p;
-	int x;
-	int len = 0;
-
-	if (str == NULL)
-		return (NULL);
-	while (str[len] != '\0')
-	{
-		len++;
-	}
-
-	p = malloc(len + 1);
-	if (p == NULL)
-
-		return (NULL);
-
-	for (x = 0; x < len; x++)
-		p[x] = str[x];
-	return (str);
-}
-
-*/
-
 
 /**
  * _strdup - main
@@ -60,16 +27,14 @@ char	*_strdup(char *str)
 	return (res);
 }
 
-
 /**
- * new_dog - function that create a new dog
+ * new_dog - dog caracteristics
  * @name: name of the dog
  * @age: age of the dog
- * @owner: owner of the dog
- *
- * Return: ndog
+ * @owner: dog's owner
+ * Return: p
  */
-/*
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ndog;
@@ -89,57 +54,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (0);
 	}
 	ndog = malloc(sizeof(dog_t));
-	if (ndog == 0)
+	if (ndog == NULL)
 	{
 		free(nname);
 		free(nowner);
 		return (0);
 	}
-	ndog->name = nname;
-	ndog->age = age;
-	ndog->owner = nowner;
-	return (ndog);
-}
-*/
-
-
-
-/**
- * new_dog - dog caracteristics
- * @name: name of the dog
- * @age: age of the dog
- * @owner: dog's owner
- * Return: p
- */
-
-dog_t *new_dog(char *name, float age, char *owner)
-{
-	dog_t *p;
-	char *n;
-	char *o;
-
-	if (name == 0 || owner == 0)
-		return (0);
-
-	n = _strdup(name);
-	if (n == 0)
-		return (0);
-	o = _strdup(owner);
-	if (o == 0)
-	{
-		free(n);
-		return (0);
-	}
-	p = malloc(sizeof(dog_t));
-	if (p == NULL)
-	{
-		free(n);
-		free(o);
-		return (0);
-	}
-	p->name = n;
+	p->name = nname;
 	p->age = age;
-	p->owner = o;
+	p->owner = nowner;
 	return (p);
 }
 
