a458e31a835dae877701fb8bc2fcf5bc2cd94ea4
simonrichard-dev <simon.insa@gmail.com>
2022-11-01 16:42:59 +0100

    fixed 4-new_dog.c script V3

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -69,7 +69,7 @@ char	*_strdup(char *str)
  *
  * Return: ndog
  */
-
+/*
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ndog;
@@ -100,3 +100,46 @@ dog_t *new_dog(char *name, float age, char *owner)
 	ndog->owner = nowner;
 	return (ndog);
 }
+*/
+
+
+
+/**
+ * new_dog - dog caracteristics
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: dog's owner
+ * Return: p
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *p;
+	char *n;
+	char *o;
+
+	if (name == 0 || owner == 0)
+		return (0);
+
+	n = _strdup(name);
+	if (n == 0)
+		return (0);
+	o = _strdup(owner);
+	if (o == 0)
+	{
+		free(n);
+		return (0);
+	}
+	p = malloc(sizeof(dog_t));
+	if (p == NULL)
+	{
+		free(n);
+		free(o);
+		return (0);
+	}
+	p->name = n;
+	p->age = age;
+	p->owner = o;
+	return (p);
+}
+
