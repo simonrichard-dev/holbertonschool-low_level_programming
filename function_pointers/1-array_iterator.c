8d486e4b96c90516183f38a33c48d8c674912aaf
simonrichard-dev <simon.insa@gmail.com>
2022-11-02 12:13:37 +0100

    fixed 1-array_iterator.c - V3

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -11,10 +11,10 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	size_t n;
+
 	if (array == 0 || size == 0 || action == 0)
 		return;
-
-	size_t n;
 
 	for (n = 0; n < size; n++)
 	action(array[n]);
