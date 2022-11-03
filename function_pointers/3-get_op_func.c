bd871bb4bb82c43a04d2b849e1ab6c40434d6467
simonrichard-dev <simon.insa@gmail.com>
2022-11-03 16:52:53 +0100

    fixed 3-get_op_func.c V1

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -2,7 +2,7 @@
 
 /**
  *get_op_func - selects the correct function
- *@s = pointer to operator
+ *@s: pointer to operator
  *
  * Return: int
  */
@@ -10,16 +10,16 @@
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        	{"+", op_add},
-        	{"-", op_sub},
-        	{"*", op_mul},
-        	{"/", op_div},
-        	{"%", op_mod},
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
 		{NULL, NULL}
 	};
 	int i = 0;
 
-	while(ops[i].op != NULL)
+	while (ops[i].op != NULL)
 	{
 		if (*ops[i].op == *s)
 			return (ops[i].f);
