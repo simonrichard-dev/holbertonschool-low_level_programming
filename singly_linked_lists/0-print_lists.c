5d3b8c0cc024ce2273a0ba1be1d162e790d006af
simonrichard-dev <simon.insa@gmail.com>
2022-11-15 12:51:46 +0100

    all files for task 0 of singly_linked_lists project

---
+++ singly_linked_lists/0-print_lists.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_list - function to print a singly linked list
+ * @h: pointer to list_t
+ *
+ * Return: number of node
+ */
+
+size_t print_list(const list_t *h)
+{
+	while
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h>str);
+		node++;
+		h = h->next;
+	}
+	return (node)
+}
