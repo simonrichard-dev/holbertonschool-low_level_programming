049c6dc9e1717106436049cb94d3cdf42dd4d947
simonrichard-dev <simon.insa@gmail.com>
2022-11-15 21:10:32 +0100

    add 2-add_node.c file and update lists.h with new prototype

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - function that adds a new node
+ * @head: pointer to list_t
+ * @str: string to print
+ *
+ * Return: node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	int i = 0;
+	char *dup;
+	list_t *new_node = 0;
+
+	dup = strdup(str);
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	if (dup == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	while (str[i])
+	{
+		i++;
+	}
+	new_node->str = dup;
+	new_node->len = i;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
