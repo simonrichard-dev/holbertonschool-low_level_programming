4614e7ddfcce3c1a804c7279cb862c4a67d6474b
simonrichard-dev <simon.insa@gmail.com>
2022-11-17 17:29:40 +0100

    3-add_nodeint_end.c - result problem

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,13 +14,19 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
+	{
 		free(new_node);
+		return (NULL);
+	}
+
 	new_node->n = n;
 	new_node->next = NULL;
 
 	if (*head == NULL)
-	{
 		*head = new_node;
+	else
+	{
+		tmp = *head;
 		while (tmp->next != NULL)
 			tmp = tmp->next;
 		tmp->next = new_node;
