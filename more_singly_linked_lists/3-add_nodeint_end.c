76255a61f566313e2fc9130a78d44932c23f44c7
simonrichard-dev <simon.insa@gmail.com>
2022-11-17 17:11:25 +0100

    3-add_nodeint_end.c - compilation problem

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,7 +12,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new_node;
 	listint_t *tmp;
 
-	new_node = malloc(sizeof(listin_t));
+	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		free(new_node);
 	new_node->n = n;
