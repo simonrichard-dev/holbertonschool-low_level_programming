62b1a8986132cc18e95ed01601b1e0cfa568704c
simonrichard-dev <simon.insa@gmail.com>
2022-11-21 12:18:55 +0100

    first commit for task 0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to POSIX
+ * @filename: pointer to filename
+ * @letters: number of letters
+ *
+ * Return: actual number of letter
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	if (!filename || !letters)
+		return (0);
+	return (1);
+}
