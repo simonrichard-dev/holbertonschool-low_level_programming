eb01cfacad97e2c23124d7077be6ae06f194f403
simonrichard-dev <simon.insa@gmail.com>
2022-11-21 15:35:55 +0100

    update for task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -10,7 +10,29 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	if (!filename || !letters)
+	ssize_t of;
+	ssize_t rf;
+	ssize_t	wf;
+	char *tmp;
+
+	tmp = malloc(sizeof(char) * letters);
+
+	if (tmp == 0)
+	{
+		free(tmp);
 		return (0);
-	return (0);
+	}
+
+	of = open(filename, O_RDONLY);
+	rf = read(of, tmp, letters);
+	wf = write(STDOUT_FILENO, tmp, rf);
+
+	if (of == -1 || rf == -1 || wf != rf)
+	{
+		free(tmp);
+		return (0);
+	}
+
+	close(of);
+	return (wf);
 }
